f9064a5a5650064f677a8d0652dadaa5a81fead6
Inesh Tandon <i08polymath@gmail.com>
2023-05-07 12:36:25 +0530

    App js update -new route added for edit component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from "./logo.svg";
 import "./App.css";
 import Home from "./components/Home";
 import Add from "./components/Add";
+import Edit from "./components/Edit";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />}></Route>
           <Route path="/create" element={<Add />} />
+          <Route path="/edit" element={<Edit />} />
         </Routes>
       </Router>
     </div>
